6ce5564b79ed62aa636a80d8c082854fd08e90a2
Zach Foster <notzachfoster@gmail.com>
2016-05-13 17:03:08 -0500

    revert to our data

--- app/components/mapbox-gl.js
+++ app/components/mapbox-gl.js
@@ -7,42 +7,36 @@ export default Ember.Component.extend({
   id: Ember.computed.alias('selector'),
 
   _initHeatMap(map) {
-    // var pointsArray = this.get('data');
-    // // var data =  {
-    // //     "type": "FeatureCollection",
-    // //     "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-    // //     "features": pointsArray
-    // // };
+    var pointsArray = this.get('data');
+    var data =  {
+        "type": "FeatureCollection",
+        "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+        "features": pointsArray
+    };
 
     map.on('load', function(){
 
         // Add a new source from our GeoJSON data and set the
         // 'cluster' option to true.
-        map.addSource("earthquakes", {
+        map.addSource("crimes", {
             type: "geojson",
-            // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
-            // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
-            data: "https://www.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson",
+            data: data,
             cluster: true,
             clusterMaxZoom: 15, // Max zoom to cluster points on
             clusterRadius: 20 // Use small cluster radius for the heatmap look
         });
 
-        // Use the earthquakes source to create four layers:
-        // three for each cluster category, and one for non-clustered markers
-
-        // Each point range gets a different fill color.
         var layers = [
             [0, 'green'],
-            [20, 'orange'],
-            [200, 'red']
+            [8, 'orange'],
+            [18, 'red']
         ];
 
         layers.forEach(function (layer, i) {
             map.addLayer({
                 "id": "cluster-" + i,
                 "type": "circle",
-                "source": "earthquakes",
+                "source": "crimes",
                 "paint": {
                     "circle-color": layer[1],
                     "circle-radius": 70,
@@ -59,7 +53,7 @@ export default Ember.Component.extend({
         map.addLayer({
             "id": "non-cluster-markers",
             "type": "circle",
-            "source": "earthquakes",
+            "source": "crimes",
             "paint": {
                 "circle-color": 'rgba(0,255,0,0.5)',
                 "circle-radius": 20,
@@ -78,7 +72,7 @@ export default Ember.Component.extend({
     mapboxgl.accessToken = 'pk.eyJ1IjoibHVjZ3JheSIsImEiOiJjaWxpM3VjcmsydHVudjZtMHR0eGYzMTd0In0.R0Hi7W-vRakpM2xmMAXbzw';
     var map = new mapboxgl.Map({
       container: selector,
-      style: 'mapbox://styles/lucgray/cili41xp900ad9gm3p45c6zxr'
+      style: 'mapbox://styles/mapbox/light-v9'
     });
 
     this._initHeatMap(map);
